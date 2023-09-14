4060e82e5aa4c17501ad3571542a05102458d12a
ferDMS <fernandodms04@gmail.com>
2023-09-14 14:31:22 -0600

    Structure graph class: 14/09
    Added data structures and a few more data types required by the DFS algo into the graph class, and started a pseudocode for the algorithm

--- zone_A/Algorithm/Graph.h
+++ zone_A/Algorithm/Graph.h
@@ -1,26 +1,84 @@
 #ifndef GRAPH
 #define GRAPH
 
+# include <vector>
+# include <stack>
+
 class Graph {
     private:
         // Number of vertices
         int num_v;
         // Vertex position of robot
         int pos;
-        // Adjacency list (edges of each vertex)
-        vector<int> adj[num_v];
+        // Adjacency array (index: vertex, values: connected vertices)
+        std::vector<std::vector<int>> adj;
+        // Stack of exploration paused vertices
+        std::stack<int> stack;
+        // Array of encountered or visited vertices
+        std::vector<bool> visited;
 
     public:
         // Constructors
-        Graph(int v);
-        // Add an undirected edge between two vertices
+        Graph(int num_v);
+        // Getters
+        int getPos();
+        std::vector<int> getEdges(int v);
+        // Setters
         void addEdge(int v1, int v2);
+        void setPos(int pos);
+        // Other methods
+        void evalPath();
 };
 
-Graph::Graph(int v) {
-    this->num_v = v;
-    // The graph expands from the starting vertex (0)
-    this->pos = 0;
+Graph::Graph(int num_v) {
+    this->num_v = num_v;
+    this->pos = 0;  // Root = 0
+    this->adj = {{}};
+    this->visited = {0};
+}
+
+int Graph::getPos() {
+    return pos;
+}
+
+std::vector<int> Graph::getEdges(int v) {
+    return adj[v];
+}
+
+void Graph::addEdge(int v1, int v2) {
+    adj[v1].push_back(v2);
+    adj[v2].push_back(v1);
+}
+
+void Graph::setPos(int pos) {
+    this->pos = pos;
+}
+
+void evalPath() {
+    /*
+    Quick pseudocode for DFS path generation
+
+    while (!stack.empty()) {
+        bool up = detectPathUp()
+        bool right = detectPathRight()
+        bool down = detectPathDown()
+        bool left = detectPathLeft()
+
+        if (right) {
+            stack.push(pos)
+            addEdge(pos, pos+1)
+            pos++
+            goRight()
+            
+        } else if (down) {
+        ""
+        } else if (up) {
+            ""
+        } else if (left) {
+            ""
+        }
+    
+    */
 }
 
 #endif
